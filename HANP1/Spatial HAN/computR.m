9d47045d22339745ad4fe03f2941372dc661333d
elotfian <109890909+elotfian@users.noreply.github.com>
2022-07-24 11:17:06 +0430

    Delete HANP1 directory

--- HANP1/Spatial HAN/computR.m
+++
@@ -1,9 +0,0 @@
-function  R=computR(xn,xc,Arc)
-
-Ctem=[Arc.cost]';
-Ctem(length(Arc)+1,:)=xc';
-Ctem(length(Arc)+2,:)=xn';
-
-
-R(1)=max(Ctem(:,1))-min(Ctem(:,1));
-R(2)=max(Ctem(:,2))-min(Ctem(:,2));
